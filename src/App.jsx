42a4c26869cb52f1777bd02f13409a7784f6e6d3
Kacie Dearman <kldearman@amarillocollege.com>
2026-07-22 04:51:54 -0500

    ✨ feat(root):
    * Introduced state management using React hooks to manage todo list data
    * Created a new TodoListItem component to handle individual todo items
    * Updated TodoList component to accept todo list data as a prop and render TodoListItem components accordingly
    * Refactored App component to utilize the new state management and TodoList component updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+const todos = [
+  {id: 1, title: "review resources"},
+  {id: 2, title: "take notes"},
+  {id: 3, title: "code out app"},
+];
 
+function App() {
+const [todoList, setTodoList] = useState(todos);
 
 
   return (
     <div className="to-do">
       <h1>Todo List</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   )
 }
