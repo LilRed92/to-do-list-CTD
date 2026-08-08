572c828790885f7c82379db4fe8cea9285d9baa6
Kacie Dearman <kldearman@amarillocollege.com>
2026-08-08 09:31:39 -0500

    ✨ feat(root):
    * Introduced state management for todo items, including completion status
    * Added lifecycle updates for todo list rendering
    * Modified TodoForm to use useState for input management
    * Updated TodoList to filter and display incomplete todos
    * Added onCompleteTodo prop to TodoListItem for handling todo completion

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,30 @@ function addTodo(todoTitle) {
   const newTodo = {
     id: Date.now(),
     title: todoTitle,
+    isCompleted: false,
   };
 
 setTodoList(previous => [newTodo, ...previous]);
+};
+
+function completeTodo(id) {
+    const updatedTodos = todoList.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+
+setTodoList(updatedTodos);
 };
 
   return (
     <div className="to-do">
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
-  )
+  );
 }
 
 export default App
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,25 @@
-import TodoListItem from './TodoListItem.jsx';
+import TodoListItem from "./TodoListItem.jsx";
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onCompleteTodo }) {
+    const filteredTodoList = todoList.filter(todo => todo.isCompleted === false);
 
-
-    return (
-        <>
+  return (
+    <>
+      {filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+      ) : (
         <ul>
-        {todoList.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
-      </ul>
-        </>
-    );
-};
+          {filteredTodoList.map((todo) => (
+            <TodoListItem
+              key={todo.id}
+              todo={todo}
+              onCompleteTodo={onCompleteTodo}
+            />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
 
 export default TodoList;
